8549479e76475dce344d1be1967dbe40d62c9daa
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2016-01-29 14:26:02 +0100

    noisy tanh neuron

--- pybrain/structure/modules/tanhlayer.py
+++ pybrain/structure/modules/tanhlayer.py
@@ -1,7 +1,7 @@
 __author__ = 'Tom Schaul, tom@idsia.ch'
 
 from scipy import tanh
-
+from numpy import random
 from pybrain.structure.modules.neuronlayer import NeuronLayer
 
 
@@ -9,7 +9,8 @@ class TanhLayer(NeuronLayer):
     """ A layer implementing the tanh squashing function. """
 
     def _forwardImplementation(self, inbuf, outbuf):
-        outbuf[:] = tanh(inbuf)
+        # outbuf[:] = tanh(inbuf)
+        outbuf[:] = tanh(inbuf) + random.normal(0., 0.01, size=inbuf.shape)
 
     def _backwardImplementation(self, outerr, inerr, outbuf, inbuf):
         inerr[:] = (1 - outbuf**2) * outerr
